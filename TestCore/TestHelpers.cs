0e5faa67fd7877cf7a9e622a8c30b81f79bf947c
S1211660 <s1211660@student.windesheim.nl>
2025-10-02 11:40:01 +0200

    feat: unit tests voor UC10
    Unit tests toegevoegd voor het testen van de onderdelen van UC10

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -9,36 +9,50 @@ namespace TestCore
         {
         }
 
-
-        //Happy flow
+        // Happy flow
         [Test]
         public void TestPasswordHelperReturnsTrue()
         {
             string password = "user3";
             string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
-            Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
+            bool result = PasswordHelper.VerifyPassword(password, passwordHash);
+            Assert.IsTrue(result);
         }
 
         [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08=")]
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=")]
-        public void TestPasswordHelperReturnsTrue(string password, string passwordHash)
+        public void TestPasswordHelperReturnsTrue_WithValidCredentials(string password, string passwordHash)
         {
-            Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
+            bool result = PasswordHelper.VerifyPassword(password, passwordHash);
+            Assert.IsTrue(result);
         }
 
-
-        //Unhappy flow
+        // Unhappy flow
         [Test]
-        public void TestPasswordHelperReturnsFalse()
+        public void TestPasswordHelperReturnsFalse_WithWrongPassword()
         {
-            Assert.Pass(); //Zelf uitwerken
+            string wrongPassword = "verkeerd_wachtwoord";
+            string correctHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
+            bool result = PasswordHelper.VerifyPassword(wrongPassword, correctHash);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TestPasswordHelperReturnsFalse_WithInvalidHashFormat()
+        {
+            string password = "user1";
+            string invalidHash = "enkeldeelzonderpunt";
+            bool result = PasswordHelper.VerifyPassword(password, invalidHash);
+            Assert.IsFalse(result);
         }
 
         [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
-        public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
+        public void TestPasswordHelperThrowsFormatException_WithInvalidBase64(string password, string passwordHash)
         {
-            Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
+            Assert.Throws<System.FormatException>(() =>
+                PasswordHelper.VerifyPassword(password, passwordHash)
+            );
         }
     }
 }
